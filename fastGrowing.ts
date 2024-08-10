8032ad4285ab931899c2d1157eeac6568b39174c
Ming <test@example.com>
2024-08-09 18:32:00 -0700

    feat: Fast growing hierarchy

---
+++ fastGrowing.ts
@@ -0,0 +1,100 @@
+import { Conway, type Real } from "./conway";
+import { mono1, unit } from "./op";
+import { eq, isOne, isZero } from "./op/comparison";
+import { canon, isLimit, pred } from "./op/ordinal";
+
+/**
+ * Evaluates the Wainer hierarchy function.
+ * @param ord The ordinal number alpha
+ * @param n The natural number in `bigint`
+ * @returns The result in `bigint`
+ */
+export const wainer = (ord: Real | Conway, n: bigint): bigint => {
+	if (isZero(ord)) {
+		return n + 1n;
+	}
+	if (isLimit(ord)) {
+		return wainer(canon(ord, n), n);
+	}
+
+	let iter = 0n;
+	const o1 = pred(ord);
+	for (let i = 0n; i < n; i += 1n) {
+		iter = wainer(o1, n);
+	}
+	return iter;
+};
+
+/**
+ * Evaluates the Hardy hierarchy function.
+ * @param ord The ordinal number alpha
+ * @param n The natural number in `bigint`
+ * @returns The result in `bigint`
+ */
+export const hardy = (ord: Real | Conway, n: bigint): bigint => {
+	// console.log('hardy', ord, n);
+	if (isZero(ord)) {
+		return n;
+	}
+	// Closed form solutions
+	if (ord instanceof Conway) {
+		if (eq(ord, unit)) {
+			return 2n * n;
+		}
+		if (ord.length === 1) {
+			if (isOne(ord.leadingPower ?? 0)) {
+				const a = BigInt(ord.leadingCoeff);
+				return n * (1n << a);
+			}
+		}
+	}
+	if (isLimit(ord)) {
+		return hardy(canon(ord, n), n);
+	}
+	return hardy(pred(ord), n + 1n);
+};
+
+/**
+ * Evaluates the slow-growing hierarchy function.
+ * @param ord The ordinal number alpha
+ * @param n The natural number in `bigint`
+ * @returns The result in `bigint`
+ */
+export const slow = (ord: Real | Conway, n: bigint): bigint => {
+	// See https://en.wikipedia.org/wiki/Slow-growing_hierarchy
+	if (isZero(ord)) {
+		return 0n;
+	}
+	if (isOne(ord)) {
+		return 1n;
+	}
+	const r = ord instanceof Conway ? ord.realValue : ord;
+	if (typeof r === "number" || typeof r === "bigint") {
+		return BigInt(r);
+	}
+	if (eq(ord, unit)) {
+		return n;
+	}
+	if (ord instanceof Conway) {
+		if (eq(ord, unit)) {
+			return n;
+		}
+		if (ord.length === 1) {
+			if (isOne(ord.leadingPower ?? 0)) {
+				const a = BigInt(ord.leadingCoeff);
+				return n + a;
+			}
+			if (isOne(ord.leadingCoeff) && !(ord.leadingPower instanceof Conway)) {
+				return n ** BigInt(ord.leadingPower || 0n);
+			}
+			if (isOne(ord.leadingCoeff) && eq(ord.leadingPower ?? 0, unit)) {
+				return n ** n;
+			}
+		}
+	}
+
+	if (isLimit(ord)) {
+		return slow(canon(ord, n), n);
+	}
+	return slow(pred(ord), n) + 1n;
+};
